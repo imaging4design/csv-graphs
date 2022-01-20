e9be61a7b88a30876633433ce553624fc2e95434
imaging4design <lovegrovemail@gmail.com>
2022-01-20 20:03:17 +1300

    Updated Labels and CSS ChartJs options

---
+++ inc/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Shoof Graphs</title>
+    <meta name="description" content="">
+    <base href="http://localhost/shoof_graphs/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="parser.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+</head>
+
+
+<body>
--- views/index-month.php
+++ views/index-month.php
@@ -1,59 +1,29 @@
-<!DOCTYPE html>
-<html>
+<?php include('../inc/header.php'); ?>
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Shoof Graphs</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="parser.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-</head>
-
-<style>
-    body {
-        font-family: "Roboto Condensed";
-    }
-
-    #graph-container {
-        padding: 10px;
-    }
-</style>
-
-<body>
-
-
+<div class="graph-wrapper">
 
     <div class="container">
         <div class="row">
-            <div class="col-sm-12">
-                <h1>ShoofDirect Sales <small>(By Month)</small></h1>
-                <p>(Minus Shipping Costs)</p>
+            <div class="col-sm-12 text-center">
+                <h1>ShoofDirect Sales <small>(By Month / Minus Shipping Costs)</small></h1>
             </div>
 
             <div class="col-lg-2 col-md-3">
-
-                <a href="index.php" class="btn btn-default">SWITCH TO YEAR</a>
-
+                <a href="index.php" class="btn btn-default">CHANGE VIEW</a>
             </div>
 
             <div class="col-lg-2 col-md-3">
 
                 <?php
                 //Get all .csv files in the csv-2021-month directory
-                $files = glob("./csv-2021-month/*.csv");
+                $files = glob("../csv-2021-month/*.csv");
 
                 echo '<select onchange="chartIt()" name="month" id="month" class="form-control">';
                 echo '<option value="">SELECT MONTH</option>';
                 foreach ($files as $file) :
-                    $raw_label = str_replace(['./csv-2021-month/', '-', '.csv'], '', $file);
+                    $raw_label = str_replace(['../csv-2021-month/', '-', '.csv'], '', $file);
                     $pretty_label = strtoupper(preg_replace('/[0-9]/', '', $raw_label));
-                    echo '<option value="' . str_replace("./csv-2021-month/", "", $file) . '">' . $pretty_label . '</option>';
+                    echo '<option value="' . str_replace("../csv-2021-month/", "", $file) . '">' . $pretty_label . '</option>';
                 endforeach;
                 echo '</select>';
 
@@ -69,13 +39,14 @@
             <div class="col-sm-12">
 
                 <div id="graph-container">
-                    <canvas id="chart" width="800" height="400"></canvas>
+                    <canvas id="chart"></canvas>
                 </div>
 
             </div>
         </div>
     </div>
 
+</div>
 
     <script>
         // chartIt()
@@ -87,7 +58,7 @@
             const data = {
                 labels: CSVdata.xValues,
                 datasets: [{
-                    label: 'ShoofDirect Sales',
+                    label: 'ShoofDirect Sales' + CSVdata.monthLabel,
                     data: CSVdata.yValues,
                     backgroundColor: ['rgba(0, 82, 147, 1)'],
                     borderColor: ['rgba(0, 82, 147, 1)'],
@@ -104,14 +75,42 @@
                 type: 'bar',
                 data,
                 options: {
+                    plugins: {  // 'legend' now within object 'plugins {}'
+                        legend: {
+                            labels: {
+                                color: "#333333",  // not 'fontColor:' anymore
+                                // fontSize: 18  // not 'fontSize:' anymore
+                                font: {
+                                    size: 18 // 'size' now within object 'font {}'
+                                }
+                            }
+                        }
+                    },
                     scales: {
                         y: {
                             ticks: {
+                                color: "#333333",
                                 // Include a dollar sign in the ticks
                                 callback: function(value, index, values) {
                                     return '$' + value;
                                 }
-                            }
+                            },
+                            grid: {
+                                color: '#EEEEEE'
+                            },
+                        },
+                        x: {
+                            title: {
+                                display: true,
+                                text: 'DAY',
+                                color: "#333333",
+                            },
+                            ticks: {
+                                color: "#333333",
+                            },
+                            grid: {
+                                color: '#EEEEEE'
+                            },
                         }
                     },
                     layout: {
@@ -155,13 +154,23 @@
 
         async function getData() {
 
-            const CSVmonth = document.getElementById("month").value;
+            // ORIGINAL CODE ...
+            //const CSVmonth = document.getElementById("month").value;
+            
+            // Set a default month for the initial page load (i.e., January)
+            let CSVmonth = null;
 
+            if(document.getElementById("month").value) {
+                CSVmonth = document.getElementById("month").value;
+            } else {
+                CSVmonth = '01-jan.csv';
+            }
+
+            // Store values ready to pass to chartIt() function
             const xValues = [];
             const yValues = [];
+            const monthLabel = ' - (' + CSVmonth.substring(3, CSVmonth.length-4).toLocaleUpperCase() + ')';
 
-
-            //const response = await fetch('./csv-2021-month/sales-aug.csv');
             const response = await fetch('./csv-2021-month/' + CSVmonth);
             const rawData = await response.text();
 
@@ -179,13 +188,25 @@
                 console.log(shipping);
             });
 
+            // Now pass values through to chartIt() function
             return {
                 xValues,
-                yValues
+                yValues,
+                monthLabel
             };
 
         }
-    </script>
-</body>
 
-</html>
+        
+        // (async() => {
+        // console.log('1')
+        // await chartIt();
+        // console.log('2')
+        // })()
+
+        chartIt();
+
+        
+    </script>
+
+<?php include('../inc/footer.php'); ?>
